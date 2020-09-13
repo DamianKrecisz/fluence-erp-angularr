6f86653954ff43608e046eaecd1c97bc0dd16d90
Nespire <33282623+Nespire@users.noreply.github.com>
2020-09-13 21:22:16 +0200

    test

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, fluence-access-token, fluence-refresh-token, _id");
-    res.setHeader('Content-Type', 'application/json');
+    req.accepts('text/html')
 
     res.header(
         'Access-Control-Expose-Headers',
